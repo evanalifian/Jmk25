6086da16652e735b5e9901d9c318fa3eb94c50a4
evanalifian <evanalifian@gmal.com>
2025-11-13 14:04:04 +0700

    membuat UserController dan halaman signup & signin

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jmk25\Controllers;
+
+use Jmk25\App\View;
+
+class UserController {
+  public static function renderSignIn() {
+    View::render("/user/signin", [
+      "title" => "Sign Up",
+      "description" => "Sign in dulu bro kalom belom!!"
+    ]);
+  }
+  
+  public static function renderSignUp() {
+    View::render("/user/signup", [
+      "title" => "Sign Up",
+      "description" => "Bikin akun dulu gak sih broo!!"
+    ]);
+  }
+}
